e05054a6eedba1baba8b27fd4b6ed58e60d33ba1
mustumak <67729712+mustumak@users.noreply.github.com>
2020-08-02 20:26:41 +0530

    dynamic display of notes.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import reactDOM from "react-dom";
 import App from "./components/App";
 
 reactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
